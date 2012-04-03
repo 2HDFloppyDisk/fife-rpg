9e4845d03d8e445f81ebf201e8292d6864a85534
Beliar <KarstenBock@gmx.net>
2012-04-03 23:56:32 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='fife-rpg',
+      version='0.1',
+      description='RPG framework use the FIFEngine',
+      author='Karsten Bock',
+      author_email='KarstenBock@gmx.net',
+      url='https://bitbucket.org/Beliar/fife-rpg',
+      packages=['fife_rpg'],
+     )
