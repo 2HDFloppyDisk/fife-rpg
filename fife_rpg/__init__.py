0788635d6ca686da5a684566bc2c0ee11da2c2b9
Beliar <KarstenBock@gmx.net>
2012-04-04 15:36:43 +0200

    Added imports of the classes to the packes __init__.py.

--- fife_rpg/__init__.py
+++ fife_rpg/__init__.py
@@ -2,3 +2,9 @@
 """The goal of fife-rpg is to create an rpg-framework using the FIFEngine
 and bGrease. It will use the source of PARPG as a basis and build upon it.
 """
+
+from map import Map
+from map import NoSuchRegionError
+from mvc import ViewBase
+from mvc import ControllerBase
+from rpg_application import RPGApplication
