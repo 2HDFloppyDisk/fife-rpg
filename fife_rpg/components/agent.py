3a7a94696e0f262fa0b90a73278b0c7e694e8f44
Beliar <KarstenBock@gmx.net>
2012-06-13 16:02:07 +0200

    Renamed graphics component to agent and added  "map". "position" and "rotation" fields.

--- fife_rpg/components/agent.py
+++ fife_rpg/components/agent.py
@@ -3,34 +3,37 @@
 #   it under the terms of the GNU General Public License as published by
 #   the Free Software Foundation, either version 3 of the License, or
 #   (at your option) any later version.
-
+#
 #   This program is distributed in the hope that it will be useful,
 #   but WITHOUT ANY WARRANTY; without even the implied warranty of
 #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #   GNU General Public License for more details.
-
+#
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """The Graphics component and functions
 
-.. module:: graphics
+.. module:: agent
     :synopsis: The Graphics component and functions
 
 .. moduleauthor:: Karsten Bock <KarstenBock@gmx.net>
 """
 
+from bGrease.geometry import Vec2d
+
 from fife_rpg.components.base import Base
 
-class Graphics(Base):
-    """Component that stores the values for the graphical representation"""
+
+class Agent(Base):
+    """Component that stores the general values of an agent"""
 
     def __init__(self):
         """Constructor"""
-        Base.__init__(self, gfx=str)
+        Base.__init__(self, gfx=str, map=str, position=Vec2d, rotation=int)
 
     @classmethod
-    def register(cls, name="graphics"):
+    def register(cls, name="agent"):
         """Registers the class as a component
 
         Args:
@@ -39,4 +42,4 @@ class Graphics(Base):
         Returns:
             True if the component was registered, False if not.
         """
-        return (super(Graphics, cls).register(name))
+        return (super(Agent, cls).register(name))
