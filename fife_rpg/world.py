5515c295f71172a9c44d98a252404c5f83915568
Beliar <KarstenBock@gmx.net>
2012-06-14 14:00:45 +0200

    Added methods to the RPGWorld to create a unique identifier for entities

--- fife_rpg/world.py
+++ fife_rpg/world.py
@@ -25,12 +25,14 @@
 from bGrease.grease_fife.world import World
 
 from fife_rpg.components import ComponentManager
-
+from fife_rpg.entities.general import General
 class RPGWorld(World):
     """The Base world for all rpgs.
     
     Sets up the generic systems and components"""
-
+    
+    MAX_ID_NUMBER = 1000
+    
     def __init__(self, engine):
         """Constructor
 
@@ -38,6 +40,38 @@ class RPGWorld(World):
             engine: a fife.Engine instance
         """
         World.__init__(self, engine)
+    
+    def is_identifier_used(self, identifier):
+        """Checks whether the idenfier is used
+        
+        Args:
+            identifier: The identifier to check
+        
+        Returns:
+            True if the identifier is used, false if not
+        """
+        entities = self[General].general.identifier == identifier
+        return len(entities) > 0
+    
+    def create_unique_identifier(self, identifier):
+        """Returns an unused identifier based on the given identifier
+        
+        Args:
+            identifier: The base identifier
+        
+        Returns:
+            A unique unused identifier based in the given identifier
+        """
+        id_number = 0
+        while self.is_identifier_used(identifier + "_" + str(id_number)):
+            id_number += 1
+            if id_number > self.MAX_ID_NUMBER:
+                raise ValueError(
+                    "Number exceeds MAX_ID_NUMBER:" + 
+                    str(self.MAX_ID_NUMBER)
+                )
+        identifier = identifier + "_" + str(id_number)
+        return identifier
     
     def configure(self):
         """Configure the worlds components and systems"""
