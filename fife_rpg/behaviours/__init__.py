faf735b29ac23b97528a402a2bf573b239b4ca73
Beliar <KarstenBock@gmx.net>
2012-11-29 16:31:07 +0100

    Added a WALK state.

--- fife_rpg/behaviours/__init__.py
+++ fife_rpg/behaviours/__init__.py
@@ -1,4 +1,11 @@
 from fife_rpg.behaviours import behaviour_manager as BehaviourManager
 from fife_rpg.helpers import Enum
 
-AGENT_STATES = Enum(["NONE", "IDLE", "APPROACH", "RUN", "WANDER", "TALK"])
+AGENT_STATES = Enum(["NONE", 
+                     "IDLE",
+                     "APPROACH",
+                     "RUN",
+                     "WALK",
+                     "WANDER",
+                     "TALK"
+                     ])
